ee8b93d292f0dbf25986fe017fee382f233b70ec
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2025-02-18 21:24:48 +0300

    Fix ClientServer.xaml

--- topchat-wpf/ClientServer.xaml.cs
+++ topchat-wpf/ClientServer.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -15,24 +16,29 @@ namespace topchat_wpf
 
 		private IMessageService _messageServiceClient;
 
+		private IDataBaseService _dataBaseService;
+
 		public Client___Server()
 		{
 			InitializeComponent();
+
+			this._dataBaseService = new DataBaseService(new DataConverterService(), new SqliteConnection());
 
 			this._providerUdp = new ConnectionProviderUdp();
 
 			this._messageServiceClient = new MessageServiceClient(new DataConverterService(), new NetworkDataService(this._providerUdp));
 		}
 
-		private void Button_Click(object sender, RoutedEventArgs e)
+		private async void Button_Click(object sender, RoutedEventArgs e)
 		{
 			UdpClient u = new UdpClient(5000);
 
 			UdpReceiveResult receivedResults = u.ReceiveAsync().Result;
 			byte[] receivedBytes = receivedResults.Buffer;
-			IPEndPoint remoteEndPoint = receivedResults.RemoteEndPoint;
 
-			ServerText.Text += $"Получено от {remoteEndPoint.Address}: {Encoding.UTF8.GetString(receivedBytes)}";
+			this._dataBaseService.AddMessage(receivedBytes);
+
+			//this._messageServiceClient.GetMessages();
 		}
 
 		private void Button_Click_1(object sender, RoutedEventArgs e)
