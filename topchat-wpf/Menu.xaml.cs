cd835cce8abd9a5b6fe5db2a28268b42ae99b516
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2025-02-24 19:06:54 +0300

    Added the client side of the server on WPF. Closed #18

--- topchat-wpf/Menu.xaml.cs
+++ topchat-wpf/Menu.xaml.cs
@@ -5,28 +5,44 @@ using TopChat.Services.Interfaces;
 
 namespace topchat_wpf
 {
-    public partial class Menu : Window
+	public partial class Menu : Window
 	{
 		private User _user;
 
-		private ADatabaseConnection _connection = new SqliteConnection();
+		private ADatabaseConnection _connection;
 
-		public Menu(User user)
+		private IConnectionProvider _connectionProvider = new ConnectionProviderUdp();
+
+		private IDataConverterService _dataConverterService = new DataConverterService();
+
+		public Menu(User user, ADatabaseConnection aDatabaseConnection, IConnectionProvider connectionProvider, IDataConverterService dataConverterService)
 		{
 			InitializeComponent();
+
 			this._user = user;
+			this._connectionProvider = connectionProvider;
+			this._connection = aDatabaseConnection;
+			this._dataConverterService = dataConverterService;
 		}
 
 		private void ClientServerButton_Click(object sender, RoutedEventArgs e)
 		{
-			Client___Server client___Server = new Client___Server();
+			Client___Server client___Server = new Client___Server(this._connectionProvider,
+				new MessageServiceClient(_dataConverterService, new NetworkDataService(this._connectionProvider)),
+				new DataBaseService(_dataConverterService, this._connection));
+
 			client___Server.Show();
 			this.Close();
 		}
 
 		private void Client_Button_Click_1(object sender, RoutedEventArgs e)
 		{
+			ClientPart client = new ClientPart(this._user, this._connectionProvider,
+				new MessageServiceClient(_dataConverterService, new NetworkDataService(this._connectionProvider)),
+				new DataBaseService(_dataConverterService, this._connection), this._connection);
 
+			client.Show();
+			this.Close();
 		}
 
 		private void ContactButton_Click_1(object sender, RoutedEventArgs e)
